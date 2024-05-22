5fb5073fafa623d75544023793daeb2f059d5d55
duartemiguel30 <dufe230804@gmail.com>
2024-05-22 21:26:16 +0100

    - GERIR TARIFAS ADICIONADO - FALTA VISUALIZAR TARIFAS

--- app/src/main/java/com/example/dai_tub/MenuAdministradorActivity.java
+++ app/src/main/java/com/example/dai_tub/MenuAdministradorActivity.java
@@ -4,8 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.LinearLayout;
-import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MenuAdministradorActivity extends AppCompatActivity {
@@ -14,6 +13,15 @@ public class MenuAdministradorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.paginainicialadministrador);
-    }
 
+        Button gerir_faturacao = findViewById(R.id.gerir_tarifas);
+        gerir_faturacao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Criar um intent para abrir a atividade da página 1
+                Intent intent = new Intent(MenuAdministradorActivity.this, MenuGerirTarifasActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
 }
